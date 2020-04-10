26f7999bd0e262fc51e453704f9b0ce90efb0ff4
Patrick Bergman <patrickbergman@me.com>
2020-04-10 17:09:35 +0200

    Wip
    Wip

---
+++ Assets/Scripts/Monster Control/MonsterMovement.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class MonsterMovement : MonoBehaviour
+{
+    public float accelerationTime = 2f;
+    public float moveSpeed = 5f;
+    private float timeLeft;
+
+    private Rigidbody2D rb;
+    private Vector2 movement;
+   
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        timeLeft -= Time.deltaTime;
+        if(timeLeft <= 0)
+        {
+            movement = new Vector2(Random.Range(-0.7f, 0.7f), Random.Range(-0.7f, 0.7f));
+            timeLeft += accelerationTime;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        rb.AddForce(movement * moveSpeed);
+    }
+
+
+}
