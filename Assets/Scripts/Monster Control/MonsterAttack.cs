322bfa8a3e57f7cd48e5490f33409caf5b6904f7
Patrick Bergman <patrickbergman@me.com>
2020-05-05 14:27:08 +0200

    Update monster prefab with movements
    Update monster prefab with movements

---
+++ Assets/Scripts/Monster Control/MonsterAttack.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class MonsterAttack : MonoBehaviour
+{
+    public float strength = 4f; // damage on hit
+    public float attackSpeed = 0.5f; // attacks per second
+    public float hitChance = 0.65f; // change the monster hits player
+
+    private Rigidbody2D rb;
+   
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        
+    }
+
+}
--- Assets/Scripts/Monster Control/MonsterMovement.cs
+++ Assets/Scripts/Monster Control/MonsterMovement.cs
@@ -1,33 +1,142 @@
 ﻿using UnityEngine;
+using System.Collections.Generic;
+
+public enum MovementTypes
+{
+    Sleep,
+    Random,
+    Horizontal,
+    Vertical,
+    ToPlayer
+}
 
 public class MonsterMovement : MonoBehaviour
 {
-    public float accelerationTime = 2f;
-    public float moveSpeed = 5f;
-    private float timeLeft;
+    public float speed = 2f;
+    public float maxRange = 4f;
+    public float viewRange = 6f;
+    public MovementTypes movementType = MovementTypes.Random;
 
     private Rigidbody2D rb;
-    private Vector2 movement;
+    private float timeLeftForDirectionChange = 2f;
+    private Vector2 movement = Vector2.zero;
+    private Vector2 initialPosition;
    
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
-    }
 
-    void Update()
-    {
-        timeLeft -= Time.deltaTime;
-        if(timeLeft <= 0)
+        initialPosition = rb.position;
+
+        Debug.Log(initialPosition);
+
+        // Cannot go directly to player on awake
+        // need to spot the player first
+        if(movementType == MovementTypes.ToPlayer)
+		{
+            movementType = MovementTypes.Random;
+		}
+
+        if (movementType == MovementTypes.Horizontal)
         {
-            movement = new Vector2(Random.Range(-0.7f, 0.7f), Random.Range(-0.7f, 0.7f));
-            timeLeft += accelerationTime;
+            List<Vector2> possibleDirections = new List<Vector2> { Vector2.up, Vector2.down };
+            movement = possibleDirections[Random.Range(0, possibleDirections.Count)];
+		}
+
+        if (movementType == MovementTypes.Vertical)
+        {
+            List<Vector2> possibleDirections = new List<Vector2> { Vector2.left, Vector2.right };
+            movement = possibleDirections[Random.Range(0, possibleDirections.Count)];
+        }
+
+        if (movementType == MovementTypes.Random)
+        {
+            List<Vector2> possibleDirections = new List<Vector2> { Vector2.left, Vector2.right, Vector2.up, Vector2.down };
+            movement = possibleDirections[Random.Range(0, possibleDirections.Count)];
         }
     }
 
     void FixedUpdate()
     {
-        rb.AddForce(movement * moveSpeed);
+        CheckMovementRangeBounds();
+        DetermineDirection();
+        DoMove();
     }
 
+    void CheckMovementRangeBounds()
+	{
+        Vector2 currentPosition = rb.position;
 
+		double distanceFromOrigin = System.Math.Sqrt(System.Math.Pow((double)(currentPosition.x - initialPosition.x), (double)2) + System.Math.Pow((double)(currentPosition.y - initialPosition.y), (double)2));
+
+		if ((float)distanceFromOrigin >= maxRange)
+		{
+			Debug.Log("Out of range");
+			Debug.Log(distanceFromOrigin);
+
+			timeLeftForDirectionChange = -1f;
+		}
+	}
+
+    void DetermineDirection()
+	{
+        if(movementType == MovementTypes.Sleep)
+		{
+            return;
+		}
+
+        timeLeftForDirectionChange -= Time.deltaTime;
+
+        if(timeLeftForDirectionChange < 0f)
+		{
+            timeLeftForDirectionChange = Random.Range(1f, 5f);
+
+            if(movementType == MovementTypes.Horizontal)
+			{
+                movement = movement == Vector2.up
+                    ? Vector2.down
+                    : Vector2.up;
+			}
+
+            if (movementType == MovementTypes.Vertical)
+            {
+                movement = movement == Vector2.left
+                    ? Vector2.right
+                    : Vector2.left;
+            }
+
+            if (movementType == MovementTypes.Random)
+            {
+                Vector2 currentDirection = movement;
+                List<Vector2> possibleDirections = new List<Vector2> { Vector2.left, Vector2.right, Vector2.up, Vector2.down };
+
+                do
+                {
+                    movement = possibleDirections[Random.Range(0, possibleDirections.Count)];
+                } while (movement == currentDirection);
+            }
+        }
+    }
+
+    void DoMove()
+	{
+        if (movementType == MovementTypes.Sleep)
+        {
+            return;
+        }
+
+        if(movementType == MovementTypes.ToPlayer)
+		{
+            MoveToPlayer();
+            return;
+		}
+
+        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+    }
+
+    void MoveToPlayer()
+	{
+        // determine where the player is.
+        // move to that position
+	}
 }
