c328ac62fe22d863b01187b53fa618b1b24f9b5c
Patrick Bergman <patrickbergman@me.com>
2020-05-08 08:44:47 +0200

    Add to monster attack script
    Add to monster attack script

--- Assets/Scripts/Monster Control/MonsterAttack.cs
+++ Assets/Scripts/Monster Control/MonsterAttack.cs
@@ -10,12 +10,48 @@ public class MonsterAttack : MonoBehaviour
    
     void Awake()
     {
+        Debug.Log("Awake!");
         rb = GetComponent<Rigidbody2D>();
     }
 
     void FixedUpdate()
     {
-        
+        Debug.Log("Fixed update");
+        if(PlayerIsCloseEnough()) {
+            Attack();
+        }
     }
 
+    bool PlayerIsCloseEnough()
+    {
+        GameObject playerObject = GameObject.FindWithTag("Player");
+        Rigidbody2D playerRigidBody = playerObject.GetComponent<Rigidbody2D>();
+
+        Vector2 currentPosition = rb.position;
+        Vector2 playerPosition = playerRigidBody.position;
+
+        double distanceFromPlayer = System.Math.Sqrt(System.Math.Pow((double)(currentPosition.x - playerPosition.x), (double)2) + System.Math.Pow((double)(currentPosition.y - playerPosition.y), (double)2));
+
+        return (float)distanceFromPlayer <= 1f;
+    }
+
+    bool TimeToAttack()
+    {
+        // something to keep track of the hit speed
+        // hit speed of 0.5 -> attack each 2 seconds
+        // hit speed of 0.1 -> attack every 10 seconds
+
+        return true;
+    }
+
+    void Attack()
+    {
+        Debug.Log("Attack!");
+        // if(TimeToAttack())
+        // {
+        //     if(Random.Range(0f, 1f) < hitChance) {
+        //         Debug.Log("Hit!");
+        //     }
+        // }
+    }
 }
